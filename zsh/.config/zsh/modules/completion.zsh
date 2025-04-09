10bd36c0ccc3201b924d8717bcd3f4f8f1ae5694
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-04-09 17:36:50 -0400

    optimizing zsh even more through lazyloading of some commands

--- zsh/.config/zsh/modules/completion.zsh
+++ zsh/.config/zsh/modules/completion.zsh
@@ -1,20 +1,13 @@
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
-
-export fpath=("$HOME/.config/zsh/functions" $fpath)
-# Load and initialise completion system
-autoload -Uz compinit
-compinit
-
-# Completion files: Use XDG dirs
-[ -d "$XDG_CACHE_HOME"/zsh ] || mkdir -p "$XDG_CACHE_HOME"/zsh
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
-compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-$ZSH_VERSION
-
-autoload bashcompinit
-bashcompinit
-source "${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion/completions/appman"
-
 eval "$(pyenv init - zsh)"
 
-compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
+export fpath=("$HOME/.config/zsh/functions" $fpath)
+
+# Completion files: Use XDG dirs
+autoload -Uz compinit
+if [[ -n ${ZDOTDIR}/.zcompdump(#qN.mh+24) ]]; then
+	compinit;
+else
+	compinit -C;
+fi;
--- zsh/.config/zsh/modules/extra.zsh
+++ zsh/.config/zsh/modules/extra.zsh
@@ -4,3 +4,22 @@ if [ -f "$LFCD" ]; then
     source "$LFCD"
     alias lf='lfcd'
 fi
+
+
+# Lazy load sdkman only when 'sdk' is called
+export SDKMAN_DIR="$XDG_DATA_HOME/sdkman"
+
+sdk() {
+  unset -f sdk
+  [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+  command sdk "$@"
+}
+
+#lazy load appman
+appman() {
+  unset -f appman
+  autoload bashcompinit
+  bashcompinit
+  source "${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion/completions/appman"
+  command appman "$@"
+}
