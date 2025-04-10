b835c30b41f1f4df2b20b56fa3f58d675f88eabc
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-04-10 13:28:30 -0400

    adding new zsh settings for using fzf

--- zsh/.config/zsh/modules/extra.zsh
+++ zsh/.config/zsh/modules/extra.zsh
@@ -27,3 +27,23 @@ appman() {
 # setting up tty stuff
 export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
+
+
+# setting up fzf settings
+# extracted from Josean Martinez video:
+# https://www.youtube.com/watch?v=mmqDYw9C30I
+
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+_fzf_compgen_path(){
+  fd --hidden --exclude .git . "$1"
+}
+
+_fzf_compgen_dir(){
+  fd --type=d --hidden --exclude .git . "$1"
+}
+
+bindkey -r '^G'
+source "$XDG_DATA_HOME/zsh/fzf-git.sh/fzf-git.sh"
