c98fe56bc2df93b2938c44bbf904d4f4ec85eaa0
marcos-aparicio <marcos.aparicio@pm.me>
2026-05-08 09:34:45 -0400

    feat(tmux,zsh): amplify sesh-fzf script to also support opening tv workmux in another keybinding

--- zsh/modules/functions.zsh
+++ zsh/modules/functions.zsh
@@ -6,7 +6,7 @@ s(){
   sesh connect "$(
     sesh list --icons | fzf-tmux -p 80%,70% \
       --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
-      --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+      --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find ^w workmux' \
       --bind 'tab:down,btab:up' \
       --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
       --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
@@ -14,8 +14,14 @@ s(){
       --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
       --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
       --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+      --bind 'ctrl-w:execute(
+        session_name=$(echo {2..} | sed "s/\x1b\[[0-9;]*m//g")
+        session_path=$(sesh list --json | jq -r ".[] | select(.Name == \"$session_name\") | .Path")
+        # Use the path in your command here
+        tv workmux $session_path
+      )+abort' \
       --preview-window 'right:55%' \
-      --preview 'sesh preview {}'
+      --preview 'sesh preview {2..}'
   )"
 }
 
