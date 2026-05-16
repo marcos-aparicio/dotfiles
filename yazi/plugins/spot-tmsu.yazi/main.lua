2e8b0338e72f37aea777ae11ec43aefc1ee650e5
marcos-aparicio <marcos.aparicio@pm.me>
2026-05-16 19:46:14 -0400

    fix(yazi/spot-tmsu): correctly show tags that have spaces in their values

--- yazi/plugins/spot-tmsu.yazi/main.lua
+++ yazi/plugins/spot-tmsu.yazi/main.lua
@@ -15,19 +15,46 @@ function M:spot(job)
     local output = handle:read('*a')
     handle:close()
 
-    -- Parse output: each line is "tag" or "tag=value" separated by spaces
-    for tag_part in output:gmatch('%S+') do
-      if tag_part ~= '' then
-        local tag, value = tag_part:match('^([^=]+)=(.*)$')
-        if tag then
-          -- Tag with value
-          table.insert(tags_section, { tag, value })
-        else
-          -- Tag without value
-          table.insert(tags_section, { tag_part, '' })
-        end
-      end
-    end
+   -- Parse output: handle escaped spaces in tags
+     -- Split by spaces, but respect escaped spaces (\ )
+     local tags = {}
+     local current_tag = ''
+     local i = 1
+     while i <= #output do
+       local char = output:sub(i, i)
+       if char == '\\' and i < #output and output:sub(i+1, i+1) == ' ' then
+         -- Escaped space - add actual space to current tag
+         current_tag = current_tag .. ' '
+         i = i + 2
+       elseif char == ' ' or char == '\n' then
+         -- Unescaped space or newline - end of tag
+         if current_tag ~= '' then
+           table.insert(tags, current_tag)
+           current_tag = ''
+         end
+         i = i + 1
+       else
+         current_tag = current_tag .. char
+         i = i + 1
+       end
+     end
+     if current_tag ~= '' then
+       table.insert(tags, current_tag)
+     end
+
+     -- Process parsed tags
+     for _, tag_part in ipairs(tags) do
+       if tag_part ~= '' then
+         local tag, value = tag_part:match('^([^=]+)=(.*)$')
+         if tag then
+           -- Tag with value
+           table.insert(tags_section, { tag, value })
+         else
+           -- Tag without value
+           table.insert(tags_section, { tag_part, '' })
+         end
+       end
+     end
   end
 
   -- Only include tags section if there are actual tags (more than just the title)
