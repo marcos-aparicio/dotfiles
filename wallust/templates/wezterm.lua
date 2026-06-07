f64f1a22aa9bf1e49fc7dc3c1f86b7e5eefabf97
marcos-aparicio <marcos.aparicio@pm.me>
2026-06-07 12:20:15 -0400

    feat(wallust): add templates for wezterm, rofi, colors.json and Xresources

---
+++ wallust/templates/wezterm.lua
@@ -0,0 +1,19 @@
+local M = {
+  foreground = "{{foreground}}",
+  background = "{{background}}",
+  cursor_bg = "{{cursor}}",
+  cursor_border = "{{cursor}}",
+  cursor_fg = "{{background}}",
+  selection_bg = "{{foreground}}",
+  selection_fg = "{{cursor}}",
+  scrollbar_thumb = "{{cursor}}",
+  split = "{{cursor}}",
+  ansi = {
+    "{{color0}}", "{{color1}}", "{{color2}}", "{{color3}}", "{{color4}}", "{{color5}}", "{{color6}}", "{{color7}}"
+  },
+  brights = {
+    "{{color8}}", "{{color9}}", "{{color10}}", "{{color11}}", "{{color12}}", "{{color13}}", "{{color14}}",
+    "{{color15}}"
+  },
+}
+return M
